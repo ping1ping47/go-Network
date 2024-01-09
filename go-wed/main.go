b2a32306b2a748401e1fed03d7d9114fb1a5a806
ping1ping47 <ping1ping47@gmail.com>
2024-01-09 14:05:26 +0700

    update rount

--- go-wed/main.go
+++ go-wed/main.go
@@ -3,22 +3,52 @@ package main
 import (
     "fmt"
     "net/http"
-	"github.com/gorilla/mux"
+	"html/template"
 )
 
+type Todo struct {
+    Title string
+    Done  bool
+}
+
+
+
+type TodoPageData struct {
+    PageTitle string
+    Todos     []Todo
+}
+
+
+
 func main() {
-	rount := mux.NewRouter()
-
-	// ex:http://localhost:80/books/cprogramming/page/1
-	// title = cprogramming
-	//page = 1
-	rount.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
-        vars := mux.Vars(r)
-        title := vars["title"]
-        page := vars["page"]
-
-        fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
+    tmpl := template.Must(template.ParseFiles("layout.html"))
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        data := TodoPageData{
+            PageTitle: "My TODO list",
+            Todos: []Todo{
+                {Title: "Task 1", Done: false},
+                {Title: "Task 2", Done: true},
+                {Title: "Task 3", Done: true},
+            },
+        }
+        tmpl.Execute(w, data)
     })
+	http.ListenAndServe(":80", nil)
+
+
+
+	// rount := mux.NewRouter()
+
+	// // ex:http://localhost:80/books/cprogramming/page/1
+	// // title = cprogramming
+	// //page = 1
+	// rount.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
+    //     vars := mux.Vars(r)
+    //     title := vars["title"]
+    //     page := vars["page"]
+
+    //     fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
+    // })
 	
 	
     // http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
@@ -30,6 +60,6 @@ func main() {
 	// ("/static/", fs))
 
 	fmt.Println("Server in runing on port 80")
-	http.ListenAndServe(":80", rount)
+	// http.ListenAndServe(":80", rount)
     // http.ListenAndServe(":80", null)
 }
